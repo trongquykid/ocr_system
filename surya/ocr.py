0e810439bc29f540726df32d5909248e92e09ff4
Trong Quy <quyht0301@gmail.com>
2024-07-18 23:40:32 +0700

    Update function and add function adjust coordinates.

--- surya/ocr.py
+++ surya/ocr.py
@@ -59,6 +59,23 @@ def run_recognition(images: List[Image.Image], langs: List[List[str]], rec_model
     return predictions_by_image
 
 
+# Increase tọa độ
+def adjust_y_coordinates(polygon, decrease_percentage, increase_percentage):
+    min_y = min(polygon, key=lambda point: point[1])[1]
+    max_y = max(polygon, key=lambda point: point[1])[1]
+
+    adjusted_polygon = []
+    for x, y in polygon:
+        if y == min_y:
+            adjusted_y = int(y * (1 - decrease_percentage / 100))
+        elif y == max_y:
+            adjusted_y = int(y * (1 + increase_percentage / 100))
+        else:
+            adjusted_y = y
+        adjusted_polygon.append([x, adjusted_y])
+    
+    return adjusted_polygon
+
 def run_ocr(images: List[Image.Image], langs: List[List[str]], det_model, det_processor, rec_model, rec_processor, batch_size=None) -> List[OCRResult]:
     images = convert_if_not_rgb(images)
     det_predictions = batch_text_detection(images, det_model, det_processor)
@@ -69,6 +86,56 @@ def run_ocr(images: List[Image.Image], langs: List[List[str]], det_model, det_pr
 
     for idx, (det_pred, image, lang) in enumerate(zip(det_predictions, images, langs)):
         polygons = [p.polygon for p in det_pred.bboxes]
+
+        polygons = [adjust_y_coordinates(polygon, 0.1, 0.2) for polygon in polygons] 
+
+        slices = slice_polys_from_image(image, polygons)
+        slice_map.append(len(slices))
+        all_langs.extend([lang] * len(slices))
+        all_slices.extend(slices)
+
+    rec_predictions, confidence_scores = batch_recognition(all_slices, all_langs, rec_model, rec_processor, batch_size=batch_size)
+
+    predictions_by_image = []
+    slice_start = 0
+    for idx, (image, det_pred, lang) in enumerate(zip(images, det_predictions, langs)):
+        slice_end = slice_start + slice_map[idx]
+        image_lines = rec_predictions[slice_start:slice_end]
+        line_confidences = confidence_scores[slice_start:slice_end]
+        slice_start = slice_end
+
+        assert len(image_lines) == len(det_pred.bboxes)
+
+        lines = []
+        for text_line, confidence, bbox in zip(image_lines, line_confidences, det_pred.bboxes):
+            lines.append(TextLine(
+                text=text_line,
+                polygon=bbox.polygon,
+                bbox=bbox.bbox,
+                confidence=confidence
+            ))
+
+        lines = sort_text_lines(lines)
+
+        predictions_by_image.append(OCRResult(
+            text_lines=lines,
+            languages=lang,
+            image_bbox=det_pred.image_bbox
+        ))
+
+    return predictions_by_image
+
+def run_ocr_v2(images: List[Image.Image], langs: List[List[str]], det_predictions, rec_model, rec_processor, batch_size=None) -> List[OCRResult]:
+
+    all_slices = []
+    slice_map = []
+    all_langs = []
+
+    for idx, (det_pred, image, lang) in enumerate(zip(det_predictions, images, langs)):
+        polygons = [p.polygon for p in det_pred.bboxes]
+
+        polygons = [adjust_y_coordinates(polygon, 0.1, 0.2) for polygon in polygons] 
+
         slices = slice_polys_from_image(image, polygons)
         slice_map.append(len(slices))
         all_langs.extend([lang] * len(slices))
