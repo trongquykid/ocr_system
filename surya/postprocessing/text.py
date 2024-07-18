7f90601744bf4bb2977ea0acc733a96d612a0b35
Trong Quy <quyht0301@gmail.com>
2024-07-18 23:22:23 +0700

    Update more thred for text.

--- surya/postprocessing/text.py
+++ surya/postprocessing/text.py
@@ -116,3 +116,29 @@ def draw_text_on_image(bboxes, texts, image_size: Tuple[int, int], langs: List[s
             render_text(draw, text, s_bbox, bbox_width, bbox_height, font_path, box_font_size)
 
     return image
+
+# Add more thred for text
+def draw_text_on_image_v2(bboxes, texts, confidences, image_size: Tuple[int, int], langs: List[str], font_path=None, max_font_size=60, res_upscale=2, has_math=False):
+    if font_path is None:
+        font_path = get_font_path(langs)
+    new_image_size = (image_size[0] * res_upscale, image_size[1] * res_upscale)
+    image = Image.new('RGB', new_image_size, color='white')
+    draw = ImageDraw.Draw(image)
+    
+    for bbox, text, confidence in zip(bboxes, texts, confidences):
+        s_bbox = [int(coord * res_upscale) for coord in bbox]
+        bbox_width = s_bbox[2] - s_bbox[0]
+        bbox_height = s_bbox[3] - s_bbox[1]
+        conf_text = round(confidence,2)
+
+        # Create thred for confidence text
+        if conf_text < 0.90: # Wrong
+            box_font_size = max(6, min(int(.75 * bbox_height), max_font_size))
+            render_text(draw, text, s_bbox, bbox_width, bbox_height, font_path, box_font_size, color="red")
+        elif 0.90 <= conf_text < 0.95: # Not sure
+            box_font_size = max(6, min(int(.75 * bbox_height), max_font_size))
+            render_text(draw, text, s_bbox, bbox_width, bbox_height, font_path, box_font_size, color="blue")
+        else: # Sure
+            box_font_size = max(6, min(int(.75 * bbox_height), max_font_size))
+            render_text(draw, text, s_bbox, bbox_width, bbox_height, font_path, box_font_size, color="black")
+    return image
